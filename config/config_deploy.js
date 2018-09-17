976de6b60689d0db59105dca4a7869b542cd87de
Elisa Kreiss <mail@elisakreiss.com>
2018-09-17 12:02:01 -0700

    create working template one trial version

---
+++ config/config_deploy.js
@@ -0,0 +1,26 @@
+var config_deploy = {
+    
+    // obligatory fields
+	
+    // needed to recover data from server app
+   "author": "ЕК",
+    // needed to recover data from server app
+   "experiment_id": "IteratedNarrationTemplate", 
+   "description": "This is the template for iterated narration experiments using the _babe framework. This template is based on Bartlett's storytelling experiments in 1932.",
+
+    // set deployment method; use one of:
+    //'debug', 'localServer', 'MTurk', 
+    // 'MTurkSandbox', 'Prolific', 'directLink'
+    // "deployMethod" : "directLink", 
+    "deployMethod" : "debug", 
+    // "deployMethod" : "MTurk", 
+    
+    // optional fields
+    
+    // who to contact in case of trouble
+    "contact_email": "ekreiss@uos.de", 
+};
+
+
+
+   
